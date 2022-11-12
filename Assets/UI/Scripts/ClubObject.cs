6efd17bfa96b02132e935ac0195fe42f7b5d1502
linusfackler <78768883+linusfackler@users.noreply.github.com>
2022-11-12 16:36:56 -0600

    Create club objects

--- Assets/UI/Scripts/ClubObject.cs
+++ Assets/UI/Scripts/ClubObject.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Category {ECS, JSOM, ATEC, MUSIC}
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/ClubObject", order = 1)]
 public class ClubObject : ScriptableObject
 {
 	public string name, president, description;
+	public List<Category> categories;
 	public Sprite icon;
 	public int memberCount;
 }
