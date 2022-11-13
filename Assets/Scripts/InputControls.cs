851fcdbaddcc2995fe5ed22864600d216b4dcdf0
Brandon Coffey <bmc939@gmail.com>
2022-11-12 18:03:19 -0600

    Networked Player

--- Assets/Scripts/InputControls.cs
+++ Assets/Scripts/InputControls.cs
@@ -1,27 +1,16 @@
 ﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputControls : MonoBehaviour
 {
-	private Controls controls;
-	
-	public Vector2 LeftAxisInput {get; private set;}
-	public Vector2 RightAxisInput {get; private set;}
+	public static Vector2 LeftAxisInput {get; private set;}
+	public static Vector2 RightAxisInput {get; private set;}
 
-	public bool LeftAction {get; private set;}
-	public bool RightAction {get; private set;}
+	public static bool LeftAction {get; private set;}
+	public static bool RightAction {get; private set;}
 	
-	
-	// Awake is called when the script instance is being loaded.
-	protected void Awake()
-	{
-		controls = new Controls();
-	}
-
-	private void Update() {
-		RightAxisInput = controls.Generic.RightAxis.ReadValue<Vector2>();
-		LeftAxisInput = controls.Generic.LeftAxis.ReadValue<Vector2>();
-		RightAction = controls.Generic.RightAction.ReadValue<bool>();
-		LeftAction = controls.Generic.LeftAction.ReadValue<bool>();
-	}
-	
+	private void OnLeftAxis(InputValue v) => LeftAxisInput = v.Get<Vector2>();
+	private void OnRightAxis(InputValue v) => RightAxisInput = v.Get<Vector2>();
+	private void OnLeftAction(InputValue  v) => LeftAction = v.isPressed;
+	private void OnRightAction(InputValue  v) => RightAction = v.isPressed;
 }
---
+++ Assets/Scripts/Networking/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+public class Player : NetworkBehaviour
+{
+	private void OnNetworkSpawn()
+	{
+		if (!IsOwner)
+		{
+			Debug.Log("Is not owner", gameObject);
+			Destroy(this);
+		}
+	}
+	
+	private void Update()
+    {
+	    var movement = (Vector3)InputControls.LeftAxisInput;
+	    transform.position += movement * Time.deltaTime;
+    }
+}
