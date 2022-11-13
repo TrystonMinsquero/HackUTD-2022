bc6d6d7975694922c709de8938b284ba40225223
Brandon Coffey <bmc939@gmail.com>
2022-11-12 18:18:05 -0600

    Fix networked Player

--- Assets/Scripts/Networking/Player.cs
+++ Assets/Scripts/Networking/Player.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using Unity.Netcode.Components;
 
 public class Player : NetworkBehaviour
 {
+	[SerializeField] private NetworkTransform _netTransform;
+	
 	public override void OnNetworkSpawn()
 	{
 		if (!IsOwner)
 		{
 			Destroy(this);
+			return;
 		}
+		if (!_netTransform) _netTransform = GetComponent<NetworkTransform>();
+		_netTransform.InLocalSpace = true;
 	}
 	
 	private void Update()
