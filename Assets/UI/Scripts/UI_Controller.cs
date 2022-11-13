ddb247ebbbc3ea7043f21bacf780b733abe07a81
Lucas Jagg <lukejagg@gmail.com>
2022-11-12 20:25:27 -0600

    Icon and Frame Rate

--- Assets/UI/Scripts/UI_Controller.cs
+++ Assets/UI/Scripts/UI_Controller.cs
@@ -10,7 +10,7 @@ public class UI_Controller : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+	    Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
