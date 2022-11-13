04d158b3dd97fe58d9e4406bdbe1702e0954f362
Lucas Jagg <lukejagg@gmail.com>
2022-11-12 22:35:43 -0600

    Events

--- Assets/UI/Scripts/EventObject.cs
+++ Assets/UI/Scripts/EventObject.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[SerializeField]
+[System.Serializable]
 public class EventObject
 {
-	
+	public string name, club, date, time, desc, location;
 }
