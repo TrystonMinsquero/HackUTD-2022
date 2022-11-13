6ab22c5f5935260a1b4de39a014fede2eb57c751
Lucas Jagg <lukejagg@gmail.com>
2022-11-13 04:06:21 -0600

    d

--- Assets/Scripts/ToborTankGame/Projectile.cs
+++ Assets/Scripts/ToborTankGame/Projectile.cs
@@ -9,12 +9,10 @@ public class Projectile : MonoBehaviour
 	[SerializeField] private float _speed;
 	[SerializeField] private float _gravity;
 	
-	public Action<NetworkObject> destroyMeDaddy = delegate(NetworkObject obj) {  };
-	
 	private void Update()
 	{
 		transform.position += (_speed * Time.deltaTime * transform.forward) + (_gravity * Time.deltaTime * -transform.up);
-		if(transform.position.y < -2) destroyMeDaddy.Invoke(GetComponent<NetworkObject>());
+	
 	}
 	
 	protected void OnTriggerEnter(Collider other)
@@ -27,6 +25,5 @@ public class Projectile : MonoBehaviour
 		{
 			Debug.Log("Destroy other tank");
 		}
-		destroyMeDaddy.Invoke(GetComponent<NetworkObject>());
 	}
 }
