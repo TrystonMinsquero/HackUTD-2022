cda739f1929769544f8dfb20a2df0e535f16c578
Brandon Coffey <bmc939@gmail.com>
2022-11-12 21:35:49 -0600

    Tobor Tank Game WIP 1

---
+++ Assets/Scripts/ToborTankGame/FollowObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+	[SerializeField] private Transform _objToFollow;
+	[SerializeField] private float _followSpeed = 10;
+	[SerializeField] private bool _rotX = true;
+	[SerializeField] private bool _rotY = true;
+	[SerializeField] private bool _rotZ = true;
+	
+	private void LateUpdate()
+	{
+		var speed = _followSpeed * Time.deltaTime;
+		transform.position = Vector3.Lerp(transform.position, _objToFollow.position, speed);
+		var orig = transform.rotation.eulerAngles;
+		var rot = Quaternion.Lerp(transform.rotation, _objToFollow.rotation, speed).eulerAngles;
+		transform.rotation = Quaternion.Euler(new Vector3(_rotX ? rot.x : orig.x, _rotY ? rot.y : orig.y, _rotZ ? rot.z : orig.z));
+    }
+}
